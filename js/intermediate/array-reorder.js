c6285449d102a504b665f25e8a81c5e68ec3ae07
Jeremy Sande <sandejeremy@gmail.com>
2017-04-12 17:19:58 -0700

    finished array reorder

--- js/intermediate/array-reorder.js
+++ js/intermediate/array-reorder.js
@@ -4,3 +4,12 @@
 // order = [3, 0, 4, 1, 2]
 // result = ['D', 'F', 'G', 'C', 'E']
 
+var fn = function(array, order) {
+	var result = [];
+	order.forEach(function(i) {
+		result.push(array[i]);
+	});
+	return result;
+}
+
+console.log(fn(['C', 'D', 'E', 'F', 'G'], [3, 0, 4, 1, 2]))

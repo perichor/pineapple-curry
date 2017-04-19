85d72884487e9b0234cc07c400cb5a58a98a80d3
Jeremy Sande <sandejeremy@gmail.com>
2017-04-18 17:04:28 -0700

    implement pig latin

--- js/intermediate/pig-latin.js
+++ js/intermediate/pig-latin.js
@@ -1,1 +1,29 @@
 // write a function that translates english to pig-latin
+
+var vowels = {
+	a: true,
+	e: true,
+	i: true,
+	o: true,
+	u: true
+};
+
+var pigLatter = function(statement) {
+	var words = statement.split(' ');
+	var result = ''
+
+	words.forEach(function(word) {
+		var letters = word.split('');
+
+		for (var i = 0; i < letters.length; i++) {
+			if (vowels[letters[i]]) {
+				result = result + word.slice(i);
+				result = result + word.slice(0, i) + 'ay ';
+				i = i + letters.length;
+			}
+		}
+	});
+	return result;
+};
+
+console.log(pigLatter('I am a duck'))
